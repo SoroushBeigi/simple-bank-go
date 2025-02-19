4ec78b229af64359ca2e1837af41a9fe0b60867d
s.masoumbeigi <sbeygi712@gmail.com>
2025-02-19 14:37:56 +0330

    feat: Implemented get account by id api

--- api/account.go
+++ api/account.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"database/sql"
+	"errors"
 	db "github.com/SoroushBeigi/simple-bank-go/db/sqlc"
 	"github.com/gin-gonic/gin"
 	"net/http"
@@ -29,5 +31,27 @@ func (server *Server) createAccount(ctx *gin.Context) {
 		return
 	}
 
+	ctx.JSON(http.StatusOK, account)
+}
+
+type getAccountRequest struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+func (server *Server) getAccountById(ctx *gin.Context) {
+	var req getAccountRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	account, err := server.store.GetAccount(ctx, req.ID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+	}
 	ctx.JSON(http.StatusOK, account)
 }
