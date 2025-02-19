ca8264aa09f8a18e956b8a0007c8e5162fa4707c
s.masoumbeigi <sbeygi712@gmail.com>
2025-02-19 16:02:34 +0330

    feat: Implemented deleteAccount api endpoint

--- api/account.go
+++ api/account.go
@@ -79,3 +79,25 @@ func (server *Server) getAccountList(ctx *gin.Context) {
 	}
 	ctx.JSON(http.StatusOK, accounts)
 }
+
+type deleteAccountRequest struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+func (server *Server) deleteAccount(ctx *gin.Context) {
+	var req deleteAccountRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	err := server.store.DeleteAccount(ctx, req.ID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+	}
+	ctx.JSON(http.StatusOK, gin.H{"message": "Account deleted successfully"})
+}
