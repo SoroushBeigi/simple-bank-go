ca5706815bdbec4fbcf8b005b78834bcd809e616
s.masoumbeigi <sbeygi712@gmail.com>
2025-02-26 15:03:49 +0330

    implemented viper to read .env configs

--- main.go
+++ main.go
@@ -4,24 +4,24 @@ import (
 	"context"
 	"github.com/SoroushBeigi/simple-bank-go/api"
 	db "github.com/SoroushBeigi/simple-bank-go/db/sqlc"
+	"github.com/SoroushBeigi/simple-bank-go/util"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"log"
 )
 
-const (
-	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := pgxpool.New(context.Background(), dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot laod config: ", err)
+	}
+	conn, err := pgxpool.New(context.Background(), config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to database: ", err)
 	}
 
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("Cannot start server: ", err)
 	}
